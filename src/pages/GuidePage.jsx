0df22a830e0dd87cc7367ad0c16bfbeb24dfcc98
DwkLoki <dwiky.darmawansyah@gmail.com>
2025-02-24 18:58:56 +0700

    tambah halaman panduan

--- src/pages/GuidePage.jsx
+++ src/pages/GuidePage.jsx
@@ -6,39 +6,57 @@ export default function GuidePage() {
             </header>
             <main className="my-4 px-6">
                 <section>
-                    <h2 className="text-xl font-bold text-zinc-600 mb-1">Ringkasan</h2>
-                    <p className="text-justify">Penjadwalan mata kuliah merupakan pekerjaan rutin yang selalu dilakukan setiap institusi pendidikan pada awal semester.
-                        Namun menciptakan suatu jadwal kuliah yang <span className="underline decoration-2 decoration-sky-500">optimal</span> cukup <span className="underline decoration-2 decoration-pink-500">sulit</span> dikarenakan banyak variabel yang saling terkait sehingga
-                        membutuhkan penanganan yang teliti. Penjadwalan kuliah di Universitas Handayani Makassar (UHM) yang ada saat ini masih
-                        dibuat secara manual, yaitu dengan pencarian kolom-kolom mana saja yang masih kosong, kemudian menempatkan jadwal pada
-                        kolom tersebut. Kepala BAAK UHM selaku pihak yang bertanggung jawab menyusun jadwal perkuliahan merasa kewalahan.
-                        Karena jadwal yang dihasilkan dengan cara seperti ini, memerlukan waktu yang lama dan memungkinkan terjadinya kesalahan
-                        dengan intensitas yang cukup banyak. Sehingga jadwal kuliah yang sudah dibuat seringkali perlu dilakukan perbaikan lagi.
-                        Karena itu butuh sebuah sistem yang mampu membuat jadwal perkuliahan yang sesuai dengan <span className="underline decoration-2 decoration-sky-500">kondisi dan kebijakan</span> perkuliahan
-                        di UHM.
+                    <h2 className="text-xl font-bold text-zinc-600 mb-1">1. Import Data Pengampu</h2>
+                    <img src="./images/panduan1.png" alt="Halaman Data Pengampu" />
+                    <p className="text-justify">
+                        Di atas merupakan tampilan halaman data pengampu. Tombol 'choose file' untuk import data pengampu.
+                        Unduh contoh file excel data pengampu pada link berikut <a href="" className="underline decoration-2 decoration-sky-500 text-sky-500">data pengampu</a>. 
+                        Anda hanya perlu menyesuaikan isi file tersebut. Jangan ubah struktur tabel datanya!
+                    </p> <br/>
+                    <p className="text-justify">
+                        Gunakan tombol 'tambah data' jika perlu menambah data pengampu satu per satu
+                    </p> <br />
+                    <p className="text-justify">
+                        Berikut tampilan data pada tabel data pengampu:
+                    </p>
+                    <img src="./images/panduan1-2.png" alt="Tabel Data Pengampu" />
+                    <div>
+                        <p className="text-justify">
+                            Tiap data pada tabel di atas, terdapat 3 tombol: edit, hapus dan pindah
+                        </p>
+                        <ul>
+                            <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Edit: untuk mengubah data pengampu</li>
+                            <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Hapus: untuk menghapus data pengampu</li>
+                            <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Pindah: untuk memindahkan data pengampu dari tabel pengampu ke tabel daftar jadwal permintaan dosen</li>
+                        </ul>
+                    </div>
+                </section>
+                <section className="mt-4">
+                    <h2 className="text-xl font-bold text-zinc-600 mb-1">2. Masukkan Data Tambahan (opsional)</h2>
+                    <img src="./images/panduan2.png" alt="Halaman Data Tambahan" />
+                    <p className="text-justify">
+                        Pada halaman data tambahan terdapat 2 tabel: <strong>tabel daftar jadwal permintaan dosen</strong> dan <strong>tabel daftar jadwal tidak bersedia mengajar dosen.</strong>
+                    </p> <br />
+                    <p className="text-justify">
+                        Jika ingin menambahkan <strong>data jadwal permintaan dosen,</strong> pilih menu data pengampu, cari data pengampu yang ingin membuat permintaan jadwal mengajar kemudian klik tombol 'pindah'.
+                        Data pengampu akan pindah ke tabel daftar jadwal permintaan dosen. Klik tombol edit pada data tersebut kemudian pilih/tentukan kapan (hari dan waktu) dan di mana (ruangan) dosen bersangkutan ingin mengajar.
+                        Tombol hapus pada data jadwal permintaan dosen akan menghapus data dari tabel dan mengembalikannya ke tabel data pengampu.
+                    </p> <br/>
+                    <p className="text-justify">
+                        Jika ingin menambahkan <strong>data jadwal tidak bersedia mengajar dosen,</strong> gunakan tombol 'tambah data', kemudian pilih nama dosen yang bersangkutan dan hari ia tidak bersedia mengajar.
                     </p>
                 </section>
                 <section className="mt-4">
-                    <h2 className="text-xl font-bold text-zinc-600 mb-1">Fitur</h2>
-                    <ul>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Penjadwalan mata kuliah sesuai kondisi dan kebijakan kampus</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Pesan jadwal: mengakomodir preferensi jadwal mengajar serta jadwal tidak bersedia mengajar dosen</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Sinkronisasi kapasitas ruangan dengan jumlah mahasiswa</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Prioritas ruangan lantai bawah bagi dosen yang berumur tua</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Sebaran jadwal perkuliahan tiap dosen</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Fitur riwayat atau penyimpanan hasil penjadwalan yang sudah diproses</li>
-                    </ul>
-                </section>
-                <section className="mt-4">
-                    <h2 className="text-xl font-bold text-zinc-600 mb-1">Kriteria penjadwalan perkuliahan di UHM</h2>
-                    <ul>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Tidak boleh ada bentrok dengan jadwal lain.</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Mata kuliah kelas “<span className="underline decoration-2 decoration-sky-500">reguler</span>” hanya dijadwalkan mulai hari <span className="underline decoration-2 decoration-sky-500">senin</span> sampai <span className="underline decoration-2 decoration-pink-500">jum’at</span> dan mulai jam <span className="underline decoration-2 decoration-sky-500">08:00</span> hingga <span className="underline decoration-2 decoration-pink-500">17:00</span> WITA.</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Mata kuliah kelas “<span className="underline decoration-2 decoration-sky-500">ekstensi</span>” dijadwalkan pada hari <span className="underline decoration-2 decoration-sky-500">sabtu</span> dan <span className="underline decoration-2 decoration-pink-500">minggu,</span> mulai jam <span className="underline decoration-2 decoration-sky-500">08:00</span> hingga <span className="underline decoration-2 decoration-pink-500">21:00</span> WITA dan tidak boleh ditempatkan di ruangan FHIS.</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Mata kuliah kelas “<span className="underline decoration-2 decoration-sky-500">malam</span>” dijadwalkan mulai hari <span className="underline decoration-2 decoration-sky-500">senin</span> sampai <span className="underline decoration-2 decoration-pink-500">jum’at</span> dan mulai jam <span className="underline decoration-2 decoration-sky-500">17:00</span> hingga <span className="underline decoration-2 decoration-pink-500">21:00</span> WITA.</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Mata kuliah kelas “<span className="underline decoration-2 decoration-sky-500">fakultas hukum</span>” dijadwalkan pada hari sabtu dan minggu, mulai jam <span className="underline decoration-2 decoration-sky-500">08:00</span> hingga <span className="underline decoration-2 decoration-pink-500">19:00</span> WITA dan diprioritaskan ditempatkan di ruangan FHIS.</li>
-                        <li className='relative text-justify pl-6 mb-1 before:absolute before:content-["‣"] before:left-0 before:-top-2 before:text-3xl before:text-sky-600'>Mata kuliah “<span className="underline decoration-2 decoration-sky-500">praktikum</span>” harus ditempatkan di ruangan Lab.</li>
-                    </ul>
+                    <h2 className="text-xl font-bold text-zinc-600 mb-1">3. Hasilkan Jadwal</h2>
+                    <img src="./images/panduan3.png" alt="Halaman Hasil Penjadwalan" />
+                    <p className="text-justify">
+                        Di atas merupakan tampilan halaman hasil penjadwalan. Saat selesai melengkapi data pengampu dan data tambahan (opsional), silahkan tekan tombol 'hasilkan jadwal'
+                        kemudian sistem akan memproses data pengampu dan data tambahan menjadi jadwal perkuliahan yang sesuai dengan kebijakan kampus yang telah ditentukan sebelumnya.
+                    </p> <br />
+                    <p className="text-justify">
+                        Selain tombol 'hasilkan jadwal' juga terdapat tombol 'reset' dan 'unduh'. Tombol 'reset' untuk menghapus seluruh hasil penjadwalan
+                        sementara tombol 'unduh' untuk mengunduh hasil penjadwalan dalam bentuk file pdf.
+                    </p> <br />
                 </section>
             </main>
         </div>
